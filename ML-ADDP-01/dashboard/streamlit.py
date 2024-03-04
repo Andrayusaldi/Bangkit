e1c580c87d28823589d876c5be46689732b26e36
Andra Yusaldi Putra <90975774+Andrayusaldi@users.noreply.github.com>
2024-03-05 03:09:56 +0700

    Update streamlit.py

--- ML-ADDP-01/dashboard/streamlit.py
+++ ML-ADDP-01/dashboard/streamlit.py
@@ -7,7 +7,7 @@ from babel.numbers import format_currency
 
 sns.set(style='dark')
 
-bike_day = pd.read_csv("day.csv")
+bike_day = pd.read_csv("../ML-ADDP-01/Bike-sharing-dataset/day.csv")
 
 # menambah label pada season
 season_labels = {
